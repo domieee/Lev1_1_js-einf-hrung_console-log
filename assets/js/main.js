acee7313eaf0380d11ebccce03469e6123af8ae6
domieee <99103235+domieee@users.noreply.github.com>
2023-01-03 13:58:47 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+// ==================== 
+// Aufgabenstellung
+// ====================
+// In dieser Übung werden wir lernen, wie die Konsole in deinem Browser funktioniert.
+// Darüber hinaus werden wir ein wenig über Variablen lernen.
+// Schreibe den untenstehenden Code in deine JavaScript Datei und sieh dir das Ergebnis mithilfe des console.log() Befehls an. Starte deinen Liveserver, um in der Konsole des Chrome-Browsers das Ergebnis zu sehen.
+// a = 12;
+// b = a * 3;
+// console.log(b);
+
+let a;
+let b;
+a = 12;
+b = a * 3;
+console.log(b);
+
+// Probiere mal, b mithilfe von document.write(b) in deinem HTML Dokument auszugeben.
+
+document.write(b);
+
+// ==================== 
+// Aufgabenstellung
+// ====================
+// Teste diesen Code in deiner JavaScript Datei.
+// console.log(document.URL);
+
+console.log(document.URL);
+
+// ==================== 
+// Aufgabenstellung
+// ====================
+// Gib ein Hello World in der Konsole aus = console.log("Hello World!");
+
+console.log("Hello World!");
+
+// Gib folgende Zeichenkette (string) in der Konsole aus: "Anton".
+// Nutze den Befehl: 
+// console.log("Anton");
+
+console.log("Anton");
+
+// Deklariere die Variable firstName mit string "Anton".
+// Gib die Variable firstName in der Konsole aus.
+// console.log(firstName);
+
+let firstName;
+firstName = "Anton";
+console.log(firstName);
+
+// Deklariere die Variable age mit dem Wert 28.
+// Gib die Variable age in der Konsole aus.
+
+let age;
+age = 28;
+console.log(age);
+
+// Deklariere die Variable job mit dem String "Trainer".
+// Gib die Variable job in der Konsole aus.
+
+let job;
+job = "Trainer";
+
+// Deklariere die Variable married mit dem boolean Wert true.
+// Nutze Befehl: let married = true;
+// Gib die Variable married in der Konsole aus.
+
+let married;
+married = true;
+console.log(married);
+
+// Zu guter Letzt wollen wir alle Variablen kombiniert in der Konsole ausgeben. Am Ende soll es so aussehen: Anton ist 28 Jahre alt, von Beruf Trainer und ist verheiratet: true
+// Hinweis: console.log(name + “ ist “ + age + “ Jahre alt “....)
+
+
